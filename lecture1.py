114ef306b5ce13d27a23f06ff893dbc8be65b859
yuliya202002 <72769862+yuliya202002@users.noreply.github.com>
2020-10-13 01:40:36 +0300

    Add files via upload

---
+++ lecture1.py
@@ -0,0 +1,13 @@
+from math import M
+
+for i in (1,2,3)
+   if =='i,j:integer' 
+while
+    else
+for i:=1 to 3 do def
+print('Введите ',i,' фамилию '==(a[i]))
+end;
+ 
+for i:=1 to 3 do def
+if (a[i][1] = 'М') or (a[i][1]='м') then
+print (i,'фамилия начинается на М (м)')
